4470dbdb820002cffb065d60ab57e79d8503c7ac
SsT307 <shaik.alhajri22@gmail.com>
2026-02-12 13:03:52 +0400

    First Commit

---
+++ src/main/java/edu/rit/iste240604group4/Iste240604Group4Application.java
@@ -0,0 +1,13 @@
+package edu.rit.iste240604group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iste240604Group4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iste240604Group4Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/rit/iste240604group4/Iste240604Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rit.iste240604group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iste240604Group4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
