3d6a5df73559d16f3639c8138087115a53f423c2
SsT307 <shaik.alhajri22@gmail.com>
2026-03-10 20:20:58 +0400

    Added a test.mustache File AND Created a Service & Controller Class WIP
    The test.mustache file is so that the template folder shows for the rest of my teammates.
    
    Added a service class, as well as a controller class as skeletons so each member can then use it and push their work into the same class.

---
+++ src/main/java/edu/rit/iste240604group4/MedProController.java
@@ -0,0 +1,20 @@
+package edu.rit.iste240604group4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MedProController {
+
+    // Reference to the service
+    @Autowired
+    private MedProService medProService;
+
+
+    // GET METHODS
+
+
+    // POST METHODS
+
+
+}
---
+++ src/main/java/edu/rit/iste240604group4/MedProService.java
@@ -0,0 +1,26 @@
+package edu.rit.iste240604group4;
+
+import edu.rit.iste240604group4.model.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MedProService {
+
+    // Simulate a DB -- one table per entity
+    public List<Patient> patients = new ArrayList<>();
+    public List<Doctor> doctors = new ArrayList<>();
+    public List<Nurse> nurses = new ArrayList<>();
+    public List<Appointment> appointments = new ArrayList<>();
+    public List<MedicalRecord> medicalRecords = new ArrayList<>();
+    public List<Hospital> hospitals = new ArrayList<>();
+
+    // Add data to the lists
+    public MedProService() {
+
+    }
+
+}
+
