7840c635010393114893d35c32d0d81c5e7fbc66
tania-2325 <ta5788@g.rit.edu>
2026-03-12 17:24:35 +0400

    Added GET & POST methods for Hospital + Medical Record pair

--- src/main/java/edu/rit/iste240604group4/MedProController.java
+++ src/main/java/edu/rit/iste240604group4/MedProController.java
@@ -1,7 +1,11 @@
 package edu.rit.iste240604group4;
 
+import edu.rit.iste240604group4.model.Hospital;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MedProController {
@@ -12,9 +16,27 @@ public class MedProController {
 
 
     // GET METHODS
+    //method to view the list of hospitals + medical records inside
+    @GetMapping("/viewHospitals")
+    public String viewAllHospitals(Model model){
+        model.addAttribute("hospitalsList", medProService.viewHospitals());
+        return "viewHospitals";
+
+    }
+    //method to view the add hospital form
+    @GetMapping("/addHospital")
+    public String showAddHospitalForm(){
+        return "addHospital";
+    }
 
 
     // POST METHODS
+    @PostMapping("/addHospital")
+    public String addHospital(Hospital h){
+        medProService.addHospital(h);
+        //"hospital" is a path variable, the success page will use it to display the message :>
+        return "redirect:/add/success/hospital";
+    }
 
 
 }
