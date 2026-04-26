24abb8717b79db9838e9606896a9bc1650fab9ff
faa3043 <faa3043@rit.edu>
2026-04-26 21:06:35 +0400

    Added data.sql for database seeding with insurance records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS iste240;
+USE iste240;
+
+DELETE FROM insurance;
+INSERT INTO insurance (insurance_name) VALUES
+                                           ('Sukoon Insurance'),
+                                           ('Dubai Insurance Company'),
+                                           ('Dubai National Insurance & Reinsurance'),
+                                           ('Orient Insurance'),
+                                           ('GIG Gulf Insurance'),
+                                           ('Daman Health Insurance'),
+                                           ('Takaful Emarat'),
+                                           ('Salama Islamic Arab Insurance'),
+                                           ('Oman Insurance Company'),
+                                           ('Union Insurance Company'),
+                                           ('Al Sagr National Insurance'),
+                                           ('Emirates Insurance Company'),
+                                           ('RAK Insurance'),
+                                           ('Adamjee Insurance'),
+                                           ('National General Insurance');
