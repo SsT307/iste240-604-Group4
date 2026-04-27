ea9ad69db320385c7e65b18b7ef8282ad8e312a4
SsT307 <shaik.alhajri22@gmail.com>
2026-04-27 06:45:10 +0400

    More Changes for patientID inclusion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -84,20 +84,20 @@ INSERT INTO patient (patient_id, emiratesid, first_name, last_name, ethnicity) V
                                                                                    (14, 784789345, 'Faisal', 'Alotaibi', 'Saudi'),
                                                                                    (15, 784012678, 'Nadine', 'Khoury', 'Syrian');
 
-INSERT INTO appointment (date, time, reason, emiratesid, doctorid, hospitalid) VALUES
-                                                                                   ('2026-03-13', '09:00:00', 'Annual checkup', 784123456, 1, 1),
-                                                                                   ('2026-03-14', '11:30:00', 'Follow-up visit', 784654321, 2, 3),
-                                                                                   ('2026-03-16', '14:00:00', 'Blood test', 784111222, 1, 2),
-                                                                                   ('2026-03-20', '08:30:00', 'Skin consultation', 784333444, 3, 4),
-                                                                                   ('2026-03-22', '10:00:00', 'Migraine review', 784555666, 2, 5),
-                                                                                   ('2026-03-24', '13:15:00', 'Allergy symptoms', 784777888, 4, 1),
-                                                                                   ('2026-03-26', '15:45:00', 'Back pain follow-up', 784999000, 1, 3),
-                                                                                   ('2026-03-28', '09:20:00', 'Eye irritation', 784111333, 3, 2),
-                                                                                   ('2026-03-30', '16:10:00', 'Joint pain consult', 784222444, 2, 4),
-                                                                                   ('2026-04-01', '12:00:00', 'Nutrition check', 784555777, 4, 5),
-                                                                                   ('2026-04-03', '10:40:00', 'ENT evaluation', 784888999, 1, 1),
-                                                                                   ('2026-04-05', '14:25:00', 'Stress management', 784123789, 3, 3),
-                                                                                   ('2026-04-07', '08:50:00', 'Hormone panel review', 784456012, 2, 2),
-                                                                                   ('2026-04-09', '11:05:00', 'Urinary discomfort', 784789345, 4, 4),
-                                                                                   ('2026-04-11', '17:00:00', 'Emergency follow-up', 784012678, 1, 5);
+INSERT INTO appointment (date, time, reason, patientid, doctorid, hospitalid) VALUES
+                                                                                   ('2026-03-13', '09:00:00', 'Annual checkup', 1, 1, 1),
+                                                                                   ('2026-03-14', '11:30:00', 'Follow-up visit', 10, 2, 3),
+                                                                                   ('2026-03-16', '14:00:00', 'Blood test', 4, 1, 2),
+                                                                                   ('2026-03-20', '08:30:00', 'Skin consultation', 5, 3, 4),
+                                                                                   ('2026-03-22', '10:00:00', 'Migraine review', 7, 2, 5),
+                                                                                   ('2026-03-24', '13:15:00', 'Allergy symptoms', 6, 4, 1),
+                                                                                   ('2026-03-26', '15:45:00', 'Back pain follow-up', 13, 1, 3),
+                                                                                   ('2026-03-28', '09:20:00', 'Eye irritation', 15, 3, 2),
+                                                                                   ('2026-03-30', '16:10:00', 'Joint pain consult', 2, 2, 4),
+                                                                                   ('2026-04-01', '12:00:00', 'Nutrition check', 3, 4, 5),
+                                                                                   ('2026-04-03', '10:40:00', 'ENT evaluation', 9, 1, 1),
+                                                                                   ('2026-04-05', '14:25:00', 'Stress management', 14, 3, 3),
+                                                                                   ('2026-04-07', '08:50:00', 'Hormone panel review', 8, 2, 2),
+                                                                                   ('2026-04-09', '11:05:00', 'Urinary discomfort', 12, 4, 4),
+                                                                                   ('2026-04-11', '17:00:00', 'Emergency follow-up', 6, 1, 5);
 
